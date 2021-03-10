a9873a4a45d0e313b789855a6bd4888b7ffc0b84
poonam-khandagle <poonam@kudosfinance.in>
2021-03-10 14:14:47 +0530

    timeout is set while entering the text value for translation

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -2,11 +2,22 @@ import React, {useState, useEffect} from "react";
 import axios from "axios";
  const Convert = ({ language, text }) => {
    const[translated,setTranslated] = useState(" ");
+   const[debouncedText,setDebouncedText] = useState("text")
+   useEffect(() => {
+     const timeoutId = setTimeout(() => {
+      setDebouncedText(text);
+     },1000);
+     //clear timeout
+     return () => {
+       clearTimeout(timeoutId);
+     }
+   },[text])
    useEffect(() => {
      const doTranslation = async() => {
-      const {data} = await axios.post("https://translation.googleapis.com/language/translate/v2",{},{
+      const {data} = await axios.post("https://translation.googleapis.com/language/translate/v2",
+      {},{
          params:{
-           q:text,
+           q:debouncedText,
            target:language.value,
            key:"AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM"
          }
@@ -14,7 +25,7 @@ import axios from "axios";
        setTranslated(data.data.translations[0].translatedText);
      }
      doTranslation();
-   },[language,text])
+   },[language,debouncedText])
    return(
      <div>
      <h1 className = "ui header">{translated}</h1>
